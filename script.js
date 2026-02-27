7abd0ee5a6ef4968f7d38490f084dd52c96bc736
SamiAchnine <satejeda99@gmail.com>
2026-02-27 13:43:06 -0500

    added proper storyline

--- script.js
+++ script.js
@@ -10,20 +10,15 @@ let sceneNumber = 1;
 function advanceScene() {
     switch (sceneNumber) {
         case 1:
+            // technically this is scene 1 and the one in the HTML by default is scene 0 but like idk man this was easier to make
             console.log("scene is " + sceneNumber);
-            caption.textContent = "I am a different caption text for scene 1";
-            image.src = "./images/forest1.avif";
+            caption.textContent = "As the temperature gets colder and colder, it eventually starts snowing. Uh oh... gotta look around to find some warmth!";
+            image.src = "./images/forest2.avif";
             sceneNumber++;
             break;
         case 2:
             console.log("scene is " + sceneNumber);
-            caption.textContent = "I am also a different caption, but this time for scene 2";
-            image.src = "./images/forest2.avif";
-            sceneNumber++;
-            break;
-        case 3:
-            console.log("scene is " + sceneNumber);
-            caption.textContent = "I am the final non-secret caption";
+            caption.textContent = "As the weather worsens, you suddenly find a random rural road! Let's hope some rando comes by and saves you from the freezing cold!";
             image.src = "./images/forest3.avif";
             sceneNumber = 'final';
             break;
@@ -32,7 +27,7 @@ function advanceScene() {
             caption.textContent = "";
             endOfPath.classList.remove('hidden');
             image.classList.add('hidden');
-            endOfPath.textContent = "Hello and welcome to a new video here on the Amazen Youtube Channel.";
+            endOfPath.textContent = "A car came by the road and picked you up before you froze! Lucky you.";
             advanceButton.disabled = true;
             break;
         default:
